39e2d812825af69fd4da2090d120ff7dd8598257
38852691 <38852691+Sharanam@users.noreply.github.com>
2021-06-05 01:34:42 +0530

    Board Logic optimized

--- Start.go
+++ Start.go
@@ -12,11 +12,13 @@ const (
 	ErrorColor   = "\033[1;31m%s\033[0m"
 	DebugColor   = "\033[0;36m%s\033[0m"
 )
-var boardData = map[int8]int8{}
+var boardData = map[int8]int8{
+	14:28,
+	13:8,
+}
 
 func main(){
 	fmt.Printf(DebugColor,"Snakes and Ladders\n")
-	boardInit()
 	fmt.Println("Value from dice: ",doDice())
 	fmt.Println("Number board:")
 	party := map[string]int8{"W":0,"X":12,"Y":4,"Z":3}
@@ -29,10 +31,6 @@ func doDice() int8{
 func boardPrint(party map[string]int8) string{
 	s, b:="",""
 	var i int8
-	var counter int8 =1
-	for i:=0; i<10; i++{
-		for j:=0;j<10;j++{
-			if party["W"] == counter{
 		for i=1; i<=100;i++{
 			if party["W"] == i{
 				b+="W" 
@@ -49,12 +47,14 @@ func boardPrint(party map[string]int8) string{
 			if b!="" {
 				fmt.Printf(WarningColor,fmt.Sprintf("%5v",b))
 			} else{
-				if boardData[i]==i{
-					fmt.Printf("%5v",i)
-				}else if boardData[i]<i{
-					fmt.Printf(ErrorColor,fmt.Sprintf("%5v",boardData[i]))
+				if  v, found :=boardData[i]; found {
+					if v<i{
+						fmt.Printf(ErrorColor,fmt.Sprintf("%5v",boardData[i]))
+					}else{
+						fmt.Printf(NoticeColor,fmt.Sprintf("%5v",boardData[i]))
+					}
 				}else{
-					fmt.Printf(NoticeColor,fmt.Sprintf("%5v",boardData[i]))
+					fmt.Printf("%5v",i)
 				}
 			}
 			b=""
@@ -63,18 +63,4 @@ func boardPrint(party map[string]int8) string{
 			}
 	}
 	return s
-}
-func boardInit(){
-	var sl=map[int8]int8{
-		14:28,
-		13:8,
-	}
-	var i int8
-	for i=1; i<=100; i++{
-		if  v, found :=sl[i]; found {
-			boardData[i]=v
-		}else{
-			boardData[i]=i
-		}
-	}
 }
